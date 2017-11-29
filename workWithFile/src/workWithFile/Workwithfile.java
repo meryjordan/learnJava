4796a8821a210bcae5c60ef76ee026dcdf3240c6
meryjodan <maruska.krat@gmail.com>
2017-11-29 15:22:11 +0100

    new project

---
+++ workWithFile/src/workWithFile/Workwithfile.java
@@ -0,0 +1,47 @@
+/*
+ * Author: Ing. Marie Kratochvilova
+ * Date: 29.11.2017
+ * Project: Create function for write even lines from file
+ *  
+ * */
+package workWithFile;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Workwithfile {
+	
+	public static void writeEvenLineFromFile() {
+		try{
+			BufferedReader inputFile = new BufferedReader(new FileReader("myFile.txt")); //load file
+			String strLine;
+			int i = 0;
+			List<String> oddLine = new ArrayList<>(); //create array list for oddLine
+			List<String> evenLine = new ArrayList<>();//create array list for evenLine
+			
+			while ((strLine = inputFile.readLine()) != null) {
+				if ((i % 2) == 0) oddLine.add(strLine); 
+				else evenLine.add(strLine);
+				i++;
+			}
+			//write even line from file
+			for (i = 0; i < evenLine.size(); i++) {
+				System.out.println(evenLine.get(i));
+			}
+			inputFile.close();
+			
+		}catch(IOException e) {
+			System.err.println("Error: " + e.getMessage());
+		}
+		
+	}
+
+	public static void main(String[] args) {
+		// call function for write even lines from file
+		writeEvenLineFromFile();
+	}
+	
+}
